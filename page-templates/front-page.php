9e99b27e176b6886b059ff80cfefd200b7257bdd
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-08-05 19:13:15 -0700

    page-templates

---
+++ page-templates/front-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * When active, by adding the heading above and providing a custom name
+ * this template becomes available in a drop-down panel in the editor.
+ *
+ * Filename can be anything.
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/#creating-custom-page-templates-for-global-use
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-front-page' );
+
+?>
+	<main id="primary" class="site-main mdl-layout__content">
+		<?php
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content/entry', 'page' );
+		}
+		?>
+	</main><!-- #primary -->
+<?php
+get_footer();
